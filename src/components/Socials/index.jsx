88099b288ceb598a61e470bf9b25fc4cf9eba80a
Marc Augier <m.augier@me.com>
2026-01-09 23:18:29 +0100

    security improvements

--- src/components/Socials/index.jsx
+++ src/components/Socials/index.jsx
@@ -11,11 +11,11 @@ function Socials() {
   return (
     <nav className="socials">
     <ul>
-      <li><a target="_blank" href="https://x.com/marcyves" className="icon"><FontAwesomeIcon icon={faTwitter} alt="X.com" /></a></li>
-      <li><a target="_blank" href="https://facebook.com/marc.augier" className="icon"><FontAwesomeIcon icon={faFacebook} alt="Facebook" /></a></li>
-      <li><a target="_blank" href="https://linkedin.com/in/marcaugier" className="icon"><FontAwesomeIcon icon={faLinkedin} alt="LinkedIn" /></a></li>
-      <li><a target="_blank" href="https://github.com/marcyves" className="icon"><FontAwesomeIcon icon={faGithub} alt="GitHub" /></a></li>
-      <li><a target="_blank" href="mailto:marc.augier@xdm-consulting.fr" className="icon"><FontAwesomeIcon icon={faEnvelope} alt="Mail" /></a></li>
+      <li><a target="_blank" rel="noopener noreferrer" href="https://x.com/marcyves" className="icon"><FontAwesomeIcon icon={faTwitter} alt="X.com" /></a></li>
+      <li><a target="_blank" rel="noopener noreferrer" href="https://facebook.com/marc.augier" className="icon"><FontAwesomeIcon icon={faFacebook} alt="Facebook" /></a></li>
+      <li><a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/marcaugier" className="icon"><FontAwesomeIcon icon={faLinkedin} alt="LinkedIn" /></a></li>
+      <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/marcyves" className="icon"><FontAwesomeIcon icon={faGithub} alt="GitHub" /></a></li>
+      <li><a target="_blank" rel="noopener noreferrer" href="mailto:marc.augier@xdm-consulting.fr" className="icon"><FontAwesomeIcon icon={faEnvelope} alt="Mail" /></a></li>
     </ul>
   </nav>
 )
